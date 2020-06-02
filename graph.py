28581b4fc156ae3e5ded6206f0ee537cc6cc324f
JacobHilbert <jacob.hilbert.tree@gmail.com>
2020-06-01 22:03:36 -0500

    Subplot spacing
    Added spacing on subplots. graph.png on the examples folder was computed 
    with the new changes.

--- graph.py
+++ graph.py
@@ -8,5 +8,6 @@ fig,ax = plt.subplots(len(sys.argv)-1)
 for i,fname in enumerate(sys.argv[1:]):
 	ax[i].plot(*(np.genfromtxt(fname).T),c='k')
 	ax[i].set_title(fname[:-4])
+fig.subplots_adjust(hspace=0.5)
 plt.savefig("graph.pdf");
 #plt.show()
