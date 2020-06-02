04128933484fa2c4eb7c32807c776ecbef22d60d
Jacob Hilbert <62489457+JacobHilbert@users.noreply.github.com>
2020-06-01 19:10:15 -0500

    Create graph.py

---
+++ graph.py
@@ -0,0 +1,12 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+fig,ax = plt.subplots(len(sys.argv)-1)
+
+for i,fname in enumerate(sys.argv[1:]):
+	ax[i].plot(*(np.genfromtxt(fname).T),c='k')
+	ax[i].set_title(fname[:-4])
+plt.savefig("graph.pdf")
+plt.show()
